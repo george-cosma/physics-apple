d7f7b75ec71237d3aa64c56a1b80fed710675003
Cosma George <georgemagic10@gmail.com>
2023-02-23 13:59:36 +0200

    Moved rendering to its own module

---
+++ src/gui.rs
@@ -0,0 +1,67 @@
+use pixels::{Pixels, SurfaceTexture};
+use winit::{
+    dpi::LogicalSize,
+    event::{Event, VirtualKeyCode},
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
+};
+use winit_input_helper::WinitInputHelper;
+
+pub const WIDTH: u32 = 480;
+pub const HEIGHT: u32 = 360;
+
+pub const SCALE: f64 = 2.0;
+
+fn build_window(event_loop: &EventLoop<()>) -> Window {
+    let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
+    let scaled_size = LogicalSize::new(WIDTH as f64 * SCALE, HEIGHT as f64 * SCALE);
+    WindowBuilder::new()
+        .with_title("Physics Apple")
+        .with_inner_size(scaled_size)
+        .with_min_inner_size(size)
+        .build(event_loop)
+        .unwrap()
+}
+
+pub fn run<F1, F2>(mut draw_function: F1, mut update_function: F2)
+where
+    F1: FnMut(&mut [u8]) + 'static,
+    F2: FnMut() + 'static,
+{
+    let event_loop = EventLoop::new();
+    let mut input = WinitInputHelper::new();
+
+    let window = build_window(&event_loop);
+    let mut pixels = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        Pixels::new(WIDTH, HEIGHT, surface_texture).unwrap()
+    };
+
+    event_loop.run(move |event, _, control_flow| {
+        // The one and only event that winit_input_helper doesn't have for us...
+        if let Event::RedrawRequested(_) = event {
+            // Request draw here
+            draw_function(pixels.get_frame_mut());
+            if let Err(err) = pixels.render() {
+                println!("[ERROR] pixels.render() failed: {}", err);
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+        }
+
+        // For everything else, for let winit_input_helper collect events to build its state.
+        // It returns `true` when it is time to update our game state and request a redraw.
+        if input.update(&event) {
+            // Close events
+            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+
+            update_function();
+
+            window.request_redraw();
+        }
+    });
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,14 @@
+use std::{cell::RefCell, rc::Rc};
+
 use clap::Parser;
 use cli::{CLIArgs, Commands};
+use gui::{HEIGHT, WIDTH};
 use physics::generate_board;
 
 mod cli;
+mod gui;
 mod physics;
 mod sequence;
-
-// Rendering {
-use pixels::{Pixels, SurfaceTexture};
-use winit::{
-    dpi::LogicalSize,
-    event::{Event, VirtualKeyCode},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
-use winit_input_helper::WinitInputHelper;
-// }
-
-const WIDTH: u32 = 480;
-const HEIGHT: u32 = 360;
 
 const USE_FPS: bool = true;
 const FPS: u128 = 30;
@@ -34,7 +24,7 @@ fn main() {
                 handle_generate(file);
             }
         }
-        Commands::SimulateFile { file: _ } => todo!(),
+        Commands::SimulateFile { file } => {}
         Commands::SimulateSequence {
             prefix: _,
             begin: _,
@@ -43,79 +33,36 @@ fn main() {
         } => todo!(),
     }
 
-    let mut board = generate_board(&"./frames/frame.0488.png".to_string());
-    board.random_particles(WIDTH * HEIGHT / 8);
+    let board_ref = Rc::new(RefCell::new(generate_board(
+        &"./frames/frame.0488.png".to_string(),
+    )));
+    board_ref.borrow_mut().random_particles(WIDTH * HEIGHT / 2);
 
-    let event_loop = EventLoop::new();
-    let mut input = WinitInputHelper::new();
-
-    let window = {
-        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
-        let scaled_size = LogicalSize::new(WIDTH as f64 * 2.0, HEIGHT as f64 * 2.0);
-        WindowBuilder::new()
-            .with_title("Physics Apple")
-            .with_inner_size(scaled_size)
-            .with_min_inner_size(size)
-            .build(&event_loop)
-            .unwrap()
-    };
-
-    let mut pixels = {
-        let window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(WIDTH, HEIGHT, surface_texture).unwrap()
-    };
-
-    event_loop.run(move |event, _, control_flow| {
-        // The one and only event that winit_input_helper doesn't have for us...
-        if let Event::RedrawRequested(_) = event {
-            // Request draw here
-            board.draw_particles(pixels.get_frame_mut());
-            if let Err(err) = pixels.render() {
-                println!("[ERROR] pixels.render() failed: {}", err);
-                *control_flow = ControlFlow::Exit;
-                return;
-            }
-        }
-
-        // For everything else, for let winit_input_helper collect events to build its state.
-        // It returns `true` when it is time to update our game state and request a redraw.
-        if input.update(&event) {
-            // Close events
-            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
-                *control_flow = ControlFlow::Exit;
-                return;
-            }
-
-            // Resize the window
-            // if let Some(size) = input.window_resized() {
-            //     if let Err(err) = pixels.resize_surface(size.width, size.height) {
-            //         println!("[ERROR] pixels.resize_surface() failed: {err}");
-            //         *control_flow = ControlFlow::Exit;
-            //         return;
-            //     }
-            // }
+    let boar_ref_clone = board_ref.clone();
+    gui::run(
+        move |buffer| {
+            board_ref.borrow().draw_particles(buffer);
+        },
+        move || {
             if USE_FPS {
                 let start = std::time::Instant::now();
                 let mut now = start.clone();
                 let mut iter = 0;
                 while (now - start).as_nanos() < (1_000_000_000 / FPS) {
-                    board.update();
+                    boar_ref_clone.borrow_mut().update();
                     now = std::time::Instant::now();
                     iter += 1;
                 }
                 println!("Ran frame for {} iterations.", iter);
             } else if USE_FIXED_ITER {
                 for _ in 0..ITER_PER_FRAME {
-                    board.update();
+                    boar_ref_clone.borrow_mut().update();
                 }
             } else {
-                board.update();
+                boar_ref_clone.borrow_mut().update();
             }
-
-            window.request_redraw();
-        }
-    });
+        },
+    );
 }
 
 fn handle_generate(filename: &String) {
